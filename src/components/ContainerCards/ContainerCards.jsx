074943051ac9a2eda7a5d83c9dd475ecfe064d46
Zahi <Zahi@MacBook-Pro-de-Cris.local>
2022-10-24 13:03:09 -0300

    Finished filter

--- src/components/ContainerCards/ContainerCards.jsx
+++ src/components/ContainerCards/ContainerCards.jsx
@@ -6,32 +6,30 @@ import jobsData from "../../assets/data.json";
 const ContainerCards = (props) => {
   const { filters, setFilters } = props;
 
+  let jobsList = jobsData.map((job) => {
+    job.categories = [...job.tools, ...job.languages, job.role, job.level];
+
+    return job;
+  });
+
+  if (filters.length > 0) {
+    jobsList = jobsList.filter((job) => {
+      return filters.every((f) => {
+        return job.categories.some((c) => f === c);
+      });
+    });
+  }
+
   return (
     <main css={container}>
-      {jobsData
-        .map((job) => {
-          job.categories = [
-            ...job.tools,
-            ...job.languages,
-            job.role,
-            job.level,
-          ];
-
-          return job;
-        })
-        .filter((job) => {
-          return job.categories.some((category) => {
-            return filters.every((filter) => filter === category);
-          });
-        })
-        .map((currentJob) => (
-          <Card
-            key={currentJob.id}
-            {...currentJob}
-            filters={filters}
-            setFilters={setFilters}
-          />
-        ))}
+      {jobsList.map((currentJob) => (
+        <Card
+          key={currentJob.id}
+          {...currentJob}
+          filters={filters}
+          setFilters={setFilters}
+        />
+      ))}
     </main>
   );
 };
